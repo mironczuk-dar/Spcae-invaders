5849201bd0fbe1f51857c8603a00e8052ccc7028
Dariusz <mironczuk.dar@gmail.com>
2026-01-11 11:15:47 +0100

    Bigger screen

--- code/main.py
+++ code/main.py
@@ -6,8 +6,8 @@ from sys import exit
 pygame.init()
 
 # Ustawienia ekranu
-SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 600
+SCREEN_WIDTH = 1000
+SCREEN_HEIGHT = 800
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Space Invaders")
 
